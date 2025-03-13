a199a29214728811d5688298e4d47eecb859d010
Abhishek <Abhishek.kothari@omunim.in>
2025-03-13 15:31:47 +0530

    Angular frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ChildComponentComponent } from './child-component/child-component.compo
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink , FormsModule , ChildComponentComponent],
+  imports: [RouterOutlet, RouterLink , FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] 
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { EditComponent } from './edit/edit.component';
 import { ListComponent } from './list/list.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
 
 export const routes: Routes = [
 
-    {path:'create',component:UserComponent},
-    {path:'edit',component:EditComponent},
+    {path:'addstudent',component:AddStudentComponent},
+    {path:'editstudent',component:EditStudentComponent},
     {path:'list',component:ListComponent}
 
 ];
